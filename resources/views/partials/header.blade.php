88ec7e656a39796279d64f726caa02b486dec236
Khairu Aqsara <khairu-aqsara@users.noreply.github.com>
2021-08-29 22:08:55 +0700

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -16,48 +16,51 @@
             <span class="sr-only">Toggle navigation</span>
         </a>
         <ul class="nav navbar-nav hidden-sm visible-lg-block">
-        {!! Admin::getNavbar()->render('left') !!}
+            {!! Admin::getNavbar()->render('left') !!}
         </ul>
 
         <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
 
-                {!! Admin::getNavbar()->render() !!}
+            {!! Admin::getNavbar()->render() !!}
 
+                @if(config('admin.navbarmode') == 'simple')
+
+                    <li><a href="{{ admin_url('auth/setting') }}">{{ Admin::user()->name }}</a> </li>
+                    <li><a href="{{ admin_url('auth/logout') }}"><i class="fa fa-sign-out"></i></a> </li>
+
+                @elseif(config('admin.navbarmode') == 'full')
                 <!-- User Account Menu -->
-                <li class="dropdown user user-menu">
-                    <!-- Menu Toggle Button -->
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <!-- The user image in the navbar-->
-                        <img src="{{ Admin::user()->avatar }}" class="user-image" alt="User Image">
-                        <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                        <span class="hidden-xs">{{ Admin::user()->name }}</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <!-- The user image in the menu -->
-                        <li class="user-header">
-                            <img src="{{ Admin::user()->avatar }}" class="img-circle" alt="User Image">
+                    <li class="dropdown user user-menu">
+                        <!-- Menu Toggle Button -->
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <!-- The user image in the navbar-->
+                            <img src="{{ Admin::user()->avatar }}" class="user-image" alt="User Image">
+                            <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                            <span class="hidden-xs">{{ Admin::user()->name }}</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <!-- The user image in the menu -->
+                            <li class="user-header">
+                                <img src="{{ Admin::user()->avatar }}" class="img-circle" alt="User Image">
 
-                            <p>
-                                {{ Admin::user()->name }}
-                                <small>Member since {{ Admin::user()->created_at }}</small>
-                            </p>
-                        </li>
-                        <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="{{ admin_url('auth/setting') }}" class="btn btn-default btn-flat">{{ trans('admin.setting') }}</a>
-                            </div>
-                            <div class="pull-right">
-                                <a href="{{ admin_url('auth/logout') }}" class="btn btn-default btn-flat">{{ trans('admin.logout') }}</a>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
-                <!-- Control Sidebar Toggle Button -->
-                {{--<li>--}}
-                    {{--<a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>--}}
-                {{--</li>--}}
+                                <p>
+                                    {{ Admin::user()->name }}
+                                    <small>Member since {{ Admin::user()->created_at }}</small>
+                                </p>
+                            </li>
+                            <li class="user-footer">
+                                <div class="pull-left">
+                                    <a href="{{ admin_url('auth/setting') }}" class="btn btn-default btn-flat">{{ trans('admin.setting') }}</a>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="{{ admin_url('auth/logout') }}" class="btn btn-default btn-flat">{{ trans('admin.logout') }}</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
             </ul>
         </div>
     </nav>
